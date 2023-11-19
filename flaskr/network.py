4c0e4a7c8ebe48c444af0571ba367a16ae989779
Santiago <santi.ari.pani@protonmail.com>
2023-11-19 20:45:43 +0100

    Update network.py

--- flaskr/network.py
+++ flaskr/network.py
@@ -8,66 +8,40 @@ class Node:
         self.port = port
         self.peers = peers
         self.context = zmq.Context()
-        self.router_socket = self.context.socket(zmq.ROUTER)
-        self.devices = {}
+        self.pub_socket = self.context.socket(zmq.PUB)
+        self.sub_sockets = {}
 
     def start(self):
-        threading.Thread(target=self.start_router_socket).start()
+        threading.Thread(target=self.start_pub_socket).start()
         for peer in self.peers:
-            threading.Thread(target=self.start_dealer_socket, args=(peer,)).start()
+            threading.Thread(target=self.start_sub_socket, args=(peer,)).start()
 
-    def start_router_socket(self):
-        self.router_socket.bind(f"tcp://*:{self.port}")
+    def start_pub_socket(self):
+        self.pub_socket.bind(f"tcp://*:{self.port}")
         print(f"Node {self.id} (You) listening on port {self.port}")
         time.sleep(1)
 
+    def start_sub_socket(self, peer):
+        sub_socket = self.context.socket(zmq.SUB)
+        sub_socket.connect(f"tcp://{peer}")
+        sub_socket.setsockopt_string(zmq.SUBSCRIBE, '')
+        self.sub_sockets[peer] = sub_socket
         while True:
-            message = self.router_socket.recv()
-            try:
-                message = message.decode('utf-8')
-            except UnicodeDecodeError:
-                continue
+            message = sub_socket.recv_string()
             print(f"Node {self.id} (You) received: {message}")
-            day_time = time.strftime("%H:%M:%S", time.localtime())
-            if message.startswith("Hello from Node"):
-                peer = message.split(" ")[3]
-                self.devices[peer] = day_time
-            elif message.endswith("is pinging you!"):
-                self.send_message_to_peer(f"{self.id} is up and running!", peer)
-            else:
-                self.send_message_to_peer(f"Node {self.id} says hi!", peer)
 
-    def start_dealer_socket(self, peer):
-        dealer_socket = self.context.socket(zmq.DEALER)
-        print(f"Node {self.id} (You) connecting to Node {peer}")
-        dealer_socket.connect(f"tcp://{peer}")
-        dealer_socket.send_string(f"Hello from Node {self.id}")
-        dealer_socket.close()
-
-    def get_devices(self):
-        return self.devices
+    def send_message(self, message):
+        self.pub_socket.send_string(message)
 
     def ping_device(self, device):
-        if device in self.devices:
+        if device in self.sub_sockets:
             print(f"Pinging device: {device}")
-            self.send_message_to_peer(f"{self.id} is pinging you!", device)
-            reply = self.router_socket.recv()
-            try:
-                reply = reply.decode('utf-8')
-            except UnicodeDecodeError:
-                return "Could not decode reply"
-            return reply
+            self.send_message(f"{self.id} is pinging you!")
         else:
-            return "Device not found"
-
-    def send_message_to_peer(self, message, peer):
-        dealer_socket = self.context.socket(zmq.DEALER)
-        dealer_socket.connect(f"tcp://{peer}")
-        dealer_socket.send_string(message)
-        dealer_socket.close()
+            print("Device not found")
 
     def join(self):
-        try:
-            self.router_socket.close()
-        finally:
-            self.context.term()
+        for sub_socket in self.sub_sockets.values():
+            sub_socket.close()
+        self.pub_socket.close()
+        self.context.term()
