bce32566284cc604597759bcea806c3f541b01eb
Santiago <santi@arias.uk>
2024-04-08 16:01:10 +0200

    Update __init__.py
    pip install waitress
    waitress-serve --host 127.0.0.1 --port 5000 --call flaskr:create_app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -33,9 +33,6 @@ def create_app(test_config=None):
 
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
