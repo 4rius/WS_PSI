1ff4260a6a8ec45ea9b3b3bc73d4a0d387ea5b5a
Santiago <santi.ari.pani@protonmail.com>
2024-04-10 22:10:34 +0200

    prioexec

--- Network/PrioritizedItem.py
+++
@@ -1,8 +0,0 @@
-# Wrappear el Future en un PrioritizedItem para que pueda ser ordenado por la cola de prioridad
-class PrioritizedItem:
-    def __init__(self, priority, item):
-        self.priority = priority
-        self.item = item
-
-    def __lt__(self, other):
-        return self.priority > other.priority
--- Network/PriorityExecutor.py
+++ Network/PriorityExecutor.py
@@ -3,7 +3,15 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 from queue import PriorityQueue
 
-from Network.PrioritizedItem import PrioritizedItem
+
+# Wrappear el Future en un PrioritizedItem para que pueda ser ordenado por la cola de prioridad
+class PrioritizedItem:
+    def __init__(self, priority, item):
+        self.priority = priority
+        self.item = item
+
+    def __lt__(self, other):
+        return self.priority > other.priority
 
 
 # Solo funciona cuando se mandan tareas de distintas prioridades a la vez, si entran 1000 iguales seguidas,
