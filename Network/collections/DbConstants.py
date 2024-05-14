bfddfcd8d9f472b29897112cb28b45817a52245b
Santiago <santi@arias.uk>
2024-05-15 01:27:43 +0200

    toying around with the primes

--- Network/collections/DbConstants.py
+++ Network/collections/DbConstants.py
@@ -1,6 +1,7 @@
 VERSION = '2.0 - DEV WS'
 
-DEFL_DOMAIN = 101
+DEFL_DOMAIN = 500
+DOM_BFV = 100003
 DEFL_SET_SIZE = 50
 DEFL_PORT = 5001
 
@@ -11,3 +12,10 @@ DEFL_EXPANSIONFACTOR = 2
 TEST_ROUNDS = 20  # This would be 20 * 6 type of operations, so 120 operations in total
 
 FB_URL = 'https://tfg-en-psi-default-rtdb.europe-west1.firebasedatabase.app'
+
+
+def update_dfl_domain(new_domain):
+    global DEFL_DOMAIN
+    DEFL_DOMAIN = new_domain
+    global DOM_BFV
+    DOM_BFV = DEFL_DOMAIN * 2 if DEFL_DOMAIN % 2 != 0 else DEFL_DOMAIN * 2 + 1
